5b0d49aa717d578042f945b6e0d393d725cc76f6
HWLima <35282201+HWLima@users.noreply.github.com>
2018-04-25 21:55:36 -0300

    Update sb-admin-charts.js

--- js/sb-admin-charts.js
+++ js/sb-admin-charts.js
@@ -83,7 +83,7 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 10,
+          max: 5,
           maxTicksLimit: 5
         },
         gridLines: {
